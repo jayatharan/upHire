485df33b7acdce3ec71da80a98ff873f4d6d650c
Indran Jayatharan <indran@calcey.com>
2022-05-03 20:02:55 +0530

    file upload

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,29 @@ import log from "./logger";
 import connect from "./db/connect";
 import router from "./routes";
 import { deserializeUser } from "./middleware";
+import { upload } from "./utils/file.util";
+import { Request, Response } from "express";
+import { get } from "lodash";
 
 const port = config.get("port") as number;
 const host = config.get("host") as string;
 
 const app = express();
+app.use('/static', express.static('static'));
+
+app.post('/upload-file',upload.single('file'),async (req:Request, res:Response) => {
+    try{
+        const file = get(req, 'file');
+        if(file){
+            res.send(file)
+        }else{
+            return res.status(400).send();
+        }
+    }catch(e){
+        log.error(e);
+        return res.status(400).send(e);
+    }
+})
 
 app.use(deserializeUser);
 app.use(express.json());
